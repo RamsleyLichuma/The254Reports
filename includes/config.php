09caac922e92342a2f9dda4c05b00105e3cfd867
Ramsley Lichuma <113148758+RamsleyLichuma@users.noreply.github.com>
2023-12-15 20:59:24 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER','terraform-20231002010411504000000008.ctjten6aavan.us-east-2.rds.amazonaws.com');
 define('DB_USER','z254User');
 define('DB_PASS' ,'Td4aokFxFM0gY6XJ1o');
-define('DB_NAME','xxxage_254report');
+define('DB_NAME','xxxage_254reports');
 
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
